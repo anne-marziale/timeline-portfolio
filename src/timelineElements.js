63a6d4e2084a932cda31b5b30343910a6815695e
anne-marziale <annemarziale@hotmail.com>
2022-03-04 11:12:47 +0100

    new project added

--- src/timelineElements.js
+++ src/timelineElements.js
@@ -1,10 +1,21 @@
 let timelineElements = [
   {
     id: 1,
-    title: "Space 3D website",
-    location: "Projet personnel",
+    title: 'Raindrops effects',
+    location: 'Projet personnel',
     description:
-      "Création d'un site avec Three.js et Vite pour avoir des animations en 3D " ,
+      "Création d'un site en Javascript afin de réaliser un effet de pluie ",
+    buttonText: 'Voir Projet',
+    link: 'https://raindrop-effect.netlify.app/',
+    date: '04/03/2022',
+    icon: 'work',
+  },
+  {
+    id: 1,
+    title: 'Space 3D website',
+    location: 'Projet personnel',
+    description:
+      "Création d'un site avec Three.js et Vite pour avoir des animations en 3D ",
     buttonText: 'Voir Projet',
     link: 'https://space-3d.netlify.app/',
     date: '03/03/2022',
@@ -13,9 +24,9 @@ let timelineElements = [
   {
     id: 1,
     title: "Page d'accueil JavaScript",
-    location: "Projet personnel",
+    location: 'Projet personnel',
     description:
-      "Création d'une page d'accueil HTML/CSS et GSAP. Utilisation de JavaScript notamment des DOM pour les animations" ,
+      "Création d'une page d'accueil HTML/CSS et GSAP. Utilisation de JavaScript notamment des DOM pour les animations",
     buttonText: 'Voir Projet',
     link: 'https://javascript-homepage.netlify.app/',
     date: '01/03/2022',
@@ -26,8 +37,8 @@ let timelineElements = [
     title: 'AngryBird Replica',
     location: 'Projet personnel',
     description:
-      "Réplique du jeu AngryBird réalisé sur la plateforme Unity avec C#.",
-    buttonText: "Voir Projet",
+      'Réplique du jeu AngryBird réalisé sur la plateforme Unity avec C#.',
+    buttonText: 'Voir Projet',
     link: 'https://simmer.io/@AnneMarziale/angrybirdsreplica',
     date: 'Octobre 2021',
     icon: 'work',
@@ -36,9 +47,8 @@ let timelineElements = [
     id: 3,
     title: 'Portfolio',
     location: 'Projet personnel',
-    description:
-      "Création d'un portfolio en ligne responsive en ReactJS.",
-    buttonText: "Voir Projet",
+    description: "Création d'un portfolio en ligne responsive en ReactJS.",
+    buttonText: 'Voir Projet',
     link: 'https://anne-marziale-portfolio.netlify.app/',
     date: 'Octobre 2021',
     icon: 'work',
@@ -48,7 +58,7 @@ let timelineElements = [
     title: 'Cine Plus',
     location: 'Projet personnel',
     description:
-    "Création d'un site de service de vidéos à la demande en utilisant une API. Réalisé avec ReactJS/CSS.",
+      "Création d'un site de service de vidéos à la demande en utilisant une API. Réalisé avec ReactJS/CSS.",
     buttonText: 'Voir Projet',
     link: 'https://javascript-homepage.netlify.app/',
     date: 'Septembre 2021',
@@ -69,10 +79,9 @@ let timelineElements = [
     id: 6,
     title: 'CV Anne Marziale',
     location: 'Projet personnel',
-    description:
-      "Réalisation d'un CV responsive avec React/CSS.",
-      buttonText: 'Voir Projet',
-      link: 'https://anne-cv.netlify.app/',
+    description: "Réalisation d'un CV responsive avec React/CSS.",
+    buttonText: 'Voir Projet',
+    link: 'https://anne-cv.netlify.app/',
     date: 'Aout 2021',
     icon: 'work',
   },
@@ -80,7 +89,8 @@ let timelineElements = [
     id: 7,
     title: "CV d'un personnage fictif : Voldemort",
     location: 'Wild Code School',
-    description: "Projet 1 | Wild Code School | Création d'un premier site web sous forme de CV responsive d'un personnage fictif en 3 semaines en HTML/CSS/JAVASCRIPT.",
+    description:
+      "Projet 1 | Wild Code School | Création d'un premier site web sous forme de CV responsive d'un personnage fictif en 3 semaines en HTML/CSS/JAVASCRIPT.",
     buttonText: 'Voir Projet',
     link: 'https://cv-tom-jedusor.netlify.app/',
     date: 'Juin 2021',
